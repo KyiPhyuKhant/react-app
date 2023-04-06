cb3688aac64edb8838969005fc3583d6bc33bfc3
Kyi <kyi@unicomi.com>
2023-04-06 15:20:36 +0630

    check age

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello Worlds</div>;
+  const age = 17;
+  const isGreen = false;
+  return (
+    <div className="App">
+      {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+      <h1 style={{ color : isGreen ? "green" : "red"}}>THIS HAS COLOR</h1>
+
+      {isGreen && <button> THIS IS BUTTIN</button>}
+    </div>
+  );
 }
 
 export default App;
