63d840e9ba3e0ae008f9aee0ca7a688ec0068b0b
Kyi <kyi@unicomi.com>
2023-04-13 17:52:21 +0630

    changed title in index

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
-      <h1 style={{ color : isGreen ? "green" : "red"}}>THIS HAS COLOR</h1>
+      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
 
       {isGreen && <button> THIS IS BUTTIN</button>}
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,28 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+if (window.innerWidth <= 768 && window.location.href.indexOf("contact-us") > -1) {
+  document.querySelectorAll("li.nav-item:not(.contact-us)").forEach(function (element) {
+    element.classList.remove("active");
+  });
+  document.querySelector("li.nav-item.contact-us").classList.add("active");
+}
+else {
+}
+
+
+if (window.innerWidth <= 768) {
+  if (window.location.href.indexOf("contact-us") > -1) {
+    var navItems = document.querySelectorAll("li.nav-item:not(.contact-us)");
+    navItems.forEach(function(navItem) {
+      navItem.classList.remove("active");
+    });
+    var contactUsNavItem = document.querySelector("li.nav-item.contact-us");
+    contactUsNavItem.classList.add("active");
+  }
+} else {
+  
+}
