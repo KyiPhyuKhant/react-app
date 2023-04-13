5ec6d0c853d658ccb10c9de93aacbce77af3934a
Kyi <kyi@unicomi.com>
2023-04-13 18:58:26 +0630

    color check function

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import './App.css';
 
 function App() {
-  const age = 17;
-  const isGreen = false;
+  const age = 19 ;
+  const isGreen = true;
   return (
     <div className="App">
       {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
