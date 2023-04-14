5f6150f4386ae9101d903f00423ce523fc30efaf
Kyi <kyi@unicomi.com>
2023-04-14 18:58:41 +0630

    lists and loop with name

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 
 import './App.css';
-import { User } from './User.js';
 
 function App() {
-  const users = [
-    { name: "Pedro", age: 21 },
-    { name: "Jake", age: 25 },
-    { name: "Jessica", age: 45 },
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
   ];
+
   return (
     <div className='App'>
-      {users.map((user, key) => {
-        return <User name={user.name} age={user.age} />;
-      })}
+      {planets.map(
+        (planet, key) => planet.isGasPlanet && <h1> {planet.name} </h1>
+      )}
     </div>
   );
 }
