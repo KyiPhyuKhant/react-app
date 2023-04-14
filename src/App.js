c747e12f6c37e51499574d93b7ad0cb0a0eb39c7
Kyi <kyi@unicomi.com>
2023-04-14 16:59:08 +0630

    created user js and imported

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
 import './App.css';
+import { User } from './User.js';
 
 function App() {
-  const names = ["Pedro", "Jake", "Jessica", "Mike", "Dustin"];
+  const users = [
+    { name: "Pedro", age: 21 },
+    { name: "Jake", age: 25 },
+    { name: "Jessica", age: 45 },
+  ];
   return (
     <div className='App'>
-      {names.map((name, key) => {
-        return <h1 key={key}>{name}</h1>
+      {users.map((user, key) => {
+        return <User name={user.name} age={user.age} />;
       })}
     </div>
   );
 }
+
+
 
 export default App;
