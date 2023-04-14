68e3e87ba10a592e349655327785f25a6a67986c
Kyi <kyi@unicomi.com>
2023-04-14 11:59:16 +0630

    loop list of names

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@
 import './App.css';
 
 function App() {
-  const age = 19 ;
-  const isGreen = true;
+  const names = ["Pedro", "Jake", "Jessica", "Mike", "Dustin"];
   return (
-    <div className="App">
-      {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
-      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
-
-      {isGreen && <button> THIS IS BUTTIN</button>}
+    <div className='App'>
+      {names.map((name, key) => {
+        return <h1 key={key}>{name}</h1>
+      })}
     </div>
   );
 }
