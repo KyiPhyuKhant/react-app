51d1698ddcc0cf5b21b0e4a1c753bdf6d62de211
Kyi <kyi@unicomi.com>
2023-04-15 19:26:34 +0630

    states in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+  // let age = 0;
+  const [age, setAge] = useState(1);
+
+  const IncreaseAge = () => {
+    setAge(5);
+    // age = age + 1;
+    // console.log(age);
+  };
 
   return (
     <div className='App'>
-      {planets.map(
-        (planet, key) => planet.isGasPlanet && <h1> {planet.name} </h1>
-      )}
-    </div>
+      {age}
+      <button onClick={IncreaseAge}>Increase age</button>
+    </div >
   );
 }
-
-
 
 export default App;
