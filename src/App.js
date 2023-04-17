9f1871df863fc1b2aedfdb2eab51b9d80100c384
Kyi <kyi@unicomi.com>
2023-04-17 10:03:30 +0630

    exercise of module 4

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [textColor, setTextColor] = useState("black");
+  // const [textColor, setTextColor] = useState("black");
+
+  // return (
+  //   <div className='App'>
+  //     <button
+  //       onClick={() => {
+  //         setTextColor(textColor === "black" ? "red" : "black");
+  //       }}
+  //     >
+  //       Show/Hide
+  //     </button>
+  //     <h1 style={{ color: textColor }}> Hi My name is kldfhkaldfh</h1>
+  //   </div >
+  // );
+
+
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  const setToZero = () => {
+    setCount(0);
+  };
 
   return (
-    <div className='App'>
-      <button
-        onClick={() => {
-          setTextColor(textColor === "black" ? "red" : "black");
-        }}
-      >
-        Show/Hide
-      </button>
-      <h1 style={{ color: textColor }}> Hi My name is kldfhkaldfh</h1>
+    <div className='App' style={{ background: '#f0f0f0', padding: '20px' }}>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={setToZero}>Set to Zero</button>
+
+  <p style={{ fontSize: '20px' }}>{count}</p>
     </div >
   );
 }
