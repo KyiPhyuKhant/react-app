d6a74c9693125993844c036235588e58c9cbfd99
Kyi <kyi@unicomi.com>
2023-04-17 16:37:51 +0630

    exercise of module 5

--- src/Module5.js
+++ src/Module5.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import {Task} from './Task';
+import { Task } from './Task';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -11,26 +11,28 @@ function App() {
   };
 
   const addTask = () => {
-    // const newTodoList = [...todoList, newTask];
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
+      complete: false,
     };
     setTodoList([...todoList, task]);
   };
 
   const deleteTask = (id) => {
-    // const newTodoList = todoList.filter((task) => {
-    //   if (task === taskName) {
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // });
-    // setTodoList(newTodoList);
-
     setTodoList(todoList.filter((task) => task.id !== id));
+  };
 
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+      );
   };
 
   return (
@@ -41,14 +43,15 @@ function App() {
       </div>
       <div className='list'>
         {todoList.map((task) => {
-          return <Task 
-          taskName={task.taskName} 
-          id={task.id}
-          deleteTask={deleteTask}
+          return <Task
+            taskName={task.taskName}
+            id={task.id}
+            completed={task.completed}
+            deleteTask={deleteTask}
+            completeTask={completeTask}
           />;
         })}
       </div>
-      {/* {newTask} */}
     </div>
   );
 }
--- src/Task.js
+++ src/Task.js
@@ -1,8 +1,9 @@
 export const Task = (props) => {
-    
+
     return (
-        <div className='task'>
+        <div className='task' style={{ backgroundColor: props.completed ? "green" : "white" }}>
             <h1>{props.taskName}</h1>
+            <button onClick={() => props.completeTask(props.id)}>Complete</button>
             <button onClick={() => props.deleteTask(props.id)}> X </button>
         </div>
     );
