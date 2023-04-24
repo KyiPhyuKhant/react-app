a93ca56e86284f202c8839415beb8378624164d9
Kyi <kyi@unicomi.com>
2023-04-24 21:53:05 +0630

    component mounted

---
+++ src/Text.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+
+export const Text = () => {
+    const [text, setText] = useState("");
+
+    useEffect(()=>{
+        console.log("component mounted");
+    });
+
+    return (
+        <div>
+            <input
+            onChange={(event) => {
+                setText(event.target.value);
+            }}
+            />
+
+            <h1>{text}</h1>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';;
 import reportWebVitals from './reportWebVitals';
+// import App from './App';;
 // import Module5 from './Module5'
 import Module6 from './Module6'
 
