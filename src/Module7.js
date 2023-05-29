bf9cdc73b15cd7b5622c51e93c6a2b820a03b19a
Kyi <kyi@unicomi.com>
2023-05-29 21:28:18 +0700

    Module 7 js file updated

--- src/Module7.js
+++ src/Module7.js
@@ -1,21 +1,12 @@
 import './App.css';
-import { useState } from 'react';
-import { Text } from './Text.js';
 
 function App() {
-    const [showText, setshowText] = useState(false);
 
     return (
         <div className='App'>
-            <button
-                onClick={() => {
-                    setshowText(!showText);
-                }}
-            >
+            <button>
                 Generate Cat Fact 
             </button>
-
-            {showText && <Text />}
         </div>
     );
 }
