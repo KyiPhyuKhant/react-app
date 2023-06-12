16784d2ef0f2c529180accf29d28324c6d64b6f1
Kyi <khantkhant528@gmail.com>
2023-06-12 23:28:23 +0700

    updated

--- src/Module7.js
+++ src/Module7.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 function App() {
+    fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) => { 
+        console.log(data);
+    });
 
     return (
         <div className='App'>
             <button>
-                Generate Cat Fact 
+                Generate Cat Fact
             </button>
         </div>
     );
